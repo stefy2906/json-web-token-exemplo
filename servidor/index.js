cf1a23ccddaf318eeae8c22c145051526ec45126
Luiz F. Picolo <luizpicolo@gmail.com>
2023-08-04 09:16:03 -0400

    Commit inicial

---
+++ servidor/index.js
@@ -0,0 +1,49 @@
+// JWT
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+var { expressjwt: expressJWT } = require("express-jwt");
+const cors = require('cors');
+
+var cookieParser = require('cookie-parser')
+
+const express = require('express');
+const { usuario } = require('./models');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+app.use(cookieParser());
+app.use(
+  expressJWT({
+    secret: process.env.SECRET,
+    algorithms: ["HS256"],
+    getToken: req => req.cookies.token
+  }).unless({ path: ["/autenticar", "/logar", "/deslogar"] })
+);
+
+app.get('/autenticar', async function(req, res){
+  res.render('autenticar');
+})
+
+app.get('/', async function(req, res){
+  res.render("home")
+})
+
+app.post('/logar', (req, res) => {
+  
+})
+
+app.post('/deslogar', function(req, res) {
+  
+})
+
+app.listen(3000, function() {
+  console.log('App de Exemplo escutando na porta 3000!')
+});
