7f2d8969353e5c4dc46dd2e0fa62c7c1fdaf4f69
stefy2906 <stefanybreve@gmail.com>
2023-08-04 16:06:29 -0400

    commit

--- servidor/config/config.js
+++ servidor/config/config.js
@@ -2,7 +2,7 @@ require('dotenv/config');
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
+    username: process.env.USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
@@ -11,7 +11,7 @@ module.exports = {
   },
 
   test: {
-    username: process.env.USERNAME,
+    username: process.env.USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
@@ -20,7 +20,7 @@ module.exports = {
   },
   
   production: {
-    username: process.env.USERNAME,
+    username: process.env.USER_NAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
