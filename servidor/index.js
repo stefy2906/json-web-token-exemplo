c2dff638e215043aee3174e44be11fa27d794be0
stefy2906 <stefanybreve@gmail.com>
2023-10-27 16:43:08 -0400

    commit

--- servidor/index.js
+++ servidor/index.js
@@ -26,7 +26,7 @@ app.use(
     secret: process.env.SECRET,
     algorithms: ["HS256"],
     getToken: req => req.cookies.token
-  }).unless({ path: ["/autenticar", "/logar", "/deslogar","/usuarios/cadastrar", "/usuarios/listar"] })
+  }).unless({ path: ["/autenticar", "/logar", "/deslogar"] })
 );
 
 app.get('/autenticar', async function(req, res){
